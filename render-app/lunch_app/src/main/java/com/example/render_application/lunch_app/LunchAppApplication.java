5682fad2323cb6776f8780b1ca916810912b9c50
Mr sultan shah <108272937+alisultanshah@users.noreply.github.com>
2023-12-17 17:18:55 +0530

    Add files via upload

---
+++ render-app/lunch_app/src/main/java/com/example/render_application/lunch_app/LunchAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.render_application.lunch_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchAppApplication.class, args);
+		System.out.println("ok");
+		
+	}
+
+}
